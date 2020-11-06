fd7f15475fc41076a94af010e38e6f0dfa595cb8
fritz-max <fritz.doermann@gmail.com>
2020-11-06 19:21:14 +0100

    Updated example App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
 
 // Example of loading a remote component
-// const RemoteApp1 = React.lazy(() => import("mfe_test/App"));
+// const RemoteApp1 = React.lazy(() => import("mfe1/App"));
+// const RemoteApp2 = React.lazy(() => import("mfe2/App"));
+
 
 const App = () => (
   <div>
@@ -11,6 +13,12 @@ const App = () => (
     <div style={{width: "1000px"}}>
       <React.Suspense fallback="Loading App">
         <RemoteApp1  />
+      </React.Suspense>
+    </div>
+    <h2>MFE-2</h2>
+    <div style={{width: "1000px"}}>
+      <React.Suspense fallback="Loading App">
+        <RemoteApp2  />
       </React.Suspense>
     </div> */}
   </div>
