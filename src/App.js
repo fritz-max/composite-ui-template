bd414e922aa3188e93df4f1650a49360b7f06070
fritz-max <fritz.doermann@gmail.com>
2020-11-06 19:04:12 +0100

    minor change

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import React from "react";
 const App = () => (
   <div>
     <h1>Prototype Composite UI</h1>
-    <p>Nothing included yet. Include components in "/composite_ui/src/App.js"</p>
+    <p>Nothing included yet. Include components in "src/App.js" of your composite UI service</p>
     {/* <h2>MFE-1</h2>
     <div style={{width: "1000px"}}>
       <React.Suspense fallback="Loading App">
