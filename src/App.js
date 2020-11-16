663e80ba0eafe1eaefd4c52a27d2918728364d46
fritz-max <fritz.doermann@gmail.com>
2020-11-16 15:48:32 +0100

    Updated configuration mechanisms

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 // Example of loading a remote component
-// const RemoteApp1 = React.lazy(() => import("mfe1/App"));
-// const RemoteApp2 = React.lazy(() => import("mfe2/App"));
+// const RemoteService1 = React.lazy(() => import("mfe1/Service"));
+// const RemoteService2 = React.lazy(() => import("mfe2/Service"));
 
 
 const App = () => (
@@ -12,13 +12,13 @@ const App = () => (
     {/* <h2>MFE-1</h2>
     <div style={{width: "1000px"}}>
       <React.Suspense fallback="Loading App">
-        <RemoteApp1  />
+        <RemoteService1  />
       </React.Suspense>
-    </div>
-    <h2>MFE-2</h2>
+    </div> */}
+    {/* <h2>MFE-2</h2>
     <div style={{width: "1000px"}}>
       <React.Suspense fallback="Loading App">
-        <RemoteApp2  />
+        <RemoteService2  />
       </React.Suspense>
     </div> */}
   </div>
